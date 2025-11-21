ba33baddcea6ee5b64a656d693d748d4b13b7777
Patricio Capparelli <Capparelli00@hotmail.com>
2025-11-21 19:05:17 -0300

    f-v-12

--- src/cine/modelo/Cliente.java
+++ src/cine/modelo/Cliente.java
@@ -2,7 +2,7 @@ package cine.modelo;
 
 import java.io.Serializable;
 
-public class Cliente implements Serializable {
+public class Cliente extends Persona implements Serializable {
     
     private static final long serialVersionUID = 1L;
     
@@ -11,13 +11,16 @@ public class Cliente implements Serializable {
     private String password; 
 
     public Cliente(String nombre, String email, String password) {
+        super(nombre, email);
         this.nombre = nombre;
         this.email = email;
         this.password = password;
     }
 
+    @Override
     public String getNombre() { return nombre; }
     
+    @Override
     public String getEmail() { return email; }
     
     public String getPassword() { return password; } 
